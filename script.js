a2a96a14270bb02c629b7ce73f32dadb3c1e1506
Konstantin <konstkam@mail.ru>
2020-11-25 00:22:57 +0300

    2nd practice in script

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 "use strict";
 
+// Часть 1. Задаём вопросы, добавляем значения
+// в personalMovieDB
+
+
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");
 
 const personalMovieDB = {
@@ -10,12 +14,30 @@ const personalMovieDB = {
       private: false
 };
 
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-      b = prompt("На сколько оцените его?", ""),
-      c = prompt("Один из последних просмотренных фильмов?", ""),
-      d = prompt("На сколько оцените его?", "");
+// Часть 2. Автоматизация вопросов юзеру с помощью циклов.
+// Практика по циклам
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+      const a = prompt("Один из последних просмотренных фильмов?", ""),
+            b = prompt("На сколько оцените его?", "");
+      
+      if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('Done');
+      } else {
+            console.log('Error');
+            i--;
+      }     
+}
+
+if (personalMovieDB.count < 10) {
+      console.log("Просмотрено слишком мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+      console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+      console.log("Вы киноман");
+} else {
+      console.log("Произошла ошибка");
+}
 
 console.log(personalMovieDB);
