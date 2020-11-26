24cd1376ad61b06f4944877fd71b0c5341da75b1
Konstantin <konstkam@mail.ru>
2020-11-26 22:49:53 +0300

    2nd practice cycles

--- script.js
+++ script.js
@@ -30,6 +30,22 @@ for (let i = 0; i < 2; i++) {
       }     
 }
 
+// let i = 0;
+
+// while (i < 2) {
+//       const a = prompt("Один из последних просмотренных фильмов?", ""),
+//             b = prompt("На сколько оцените его?", "");
+      
+//       if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//             personalMovieDB.movies[a] = b;
+//             console.log('Done');
+//             i++;
+//       } else {
+//             console.log('Error');
+//             continue;
+//       }     
+// }
+
 if (personalMovieDB.count < 10) {
       console.log("Просмотрено слишком мало фильмов");
 } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
