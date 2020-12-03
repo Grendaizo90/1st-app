8c4838fa07b4e3279edd177116c0cc1c41c27be3
Konstantin <konstkam@mail.ru>
2020-12-03 23:08:47 +0300

    Added methods lesson

---
+++ methods.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// Strings
+const str = "teSt";
+const arr = [1, 2, 4];
+
+// console.log(str[2] = 'd');
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str);
+
+const fruit = "Some fruit";
+console.log(fruit.indexOf('d'));
+console.log(fruit.indexOf('fruit'));
+
+const logg = "Hello world";
+console.log(logg.slice(6, 11));
+console.log(logg.substring(6, 10));
+console.log(logg.substr(6, 2));
+
+// Integers
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = "12.2px";
+console.log(parseInt(test));
+console.log(parseFloat(test));
