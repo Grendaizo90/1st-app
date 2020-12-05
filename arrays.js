5c0f85fdc6c8687f23c9b4d843d2b25a28066538
Konstantin <konstkam@mail.ru>
2020-12-05 17:31:36 +0300

    Added arrays lesson

---
+++ arrays.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const arr = [56, 2, 10, 62, 8];
+arr.sort(compareNum);
+console.log(arr);
+
+function compareNum(a, b) {
+    return a - b;
+}
+
+
+// arr[99] = 0;
+// console.log(arr.length);
+// console.log(arr);
+
+// arr.forEach(function(item, i, arr) {
+//     console.log(`${i}: ${item} внутри массива ${arr}`);
+// });
+
+// // arr.pop();
+// arr.push(10);
+// console.log(arr);
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// for (let value of arr) {
+//     console.log(value);
+// }
+
+// const str = prompt("", "");
+// const products = str.split(", ");
+// products.sort();
+// console.log(products.join('; '));
